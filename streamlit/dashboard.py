a5c245dc962f87a0360ef63e119dc6e9cae19a0b
3Malaika <babouma.suzanne@institutsaintjean.org>
2026-04-22 16:25:27 +0100

    je commit dashboard

--- streamlit/dashboard.py
+++ streamlit/dashboard.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 import streamlit as st
-
+import seaborn as sns
+import time
 
 df = pd.read_csv('bank.csv')
 st.set_page_config(page_title='Real Time Scence Dashboard', page_icon='+',layout='wide')
@@ -14,3 +15,26 @@ st.title('Real Time Data Analytics')
 # filtre sur les jobs 
 job_filter = st.selectbox('Select a job', pd.unique(df['job']))
 df = df[df['job']== job_filter]
+
+avg_age = np.mean(df['age'])
+count_married = int(df[(df['marital'] == 'married')]['marital'].count())
+balance = np.mean(df['balance'])
+kpi1,kpi2,kpi3 = st.columns(3)
+kpi1.metric(label='Age',value=round(avg_age),delta=round(avg_age))
+kpi2.metric(label='Married_count',value=count_married, delta=round(count_married))
+kpi3.metric(label='Balance',value=f"${round(balance,2)}", delta=round(balance/count_married)*100)
+
+col1,col2 = st.columns(2)
+with col1:
+    st.markdown('premier chart')
+    fig1 = plt.figure()
+    sns.barplot(data=df, y='age', x='marital',palette='muted')
+    st.pyplot(fig1)
+with col2:
+    st.markdown('Second chart')
+    fig2 = plt.figure()
+    sns.histplot(data=df,  x='age')
+    st.pyplot(fig2)
+st.markdown('Vue détaillée')
+st.dataframe(df)
+time.sleep(1)
