618fa46ac43433a6ed9e6651af60b6f271dffa2c
3Malaika <babouma.suzanne@institutsaintjean.org>
2026-04-23 16:33:15 +0100

    add quatriemeSeance

---
+++ quatriemeSeance/app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(layout='wide')
+
+import pandas as pd
+from pathlib import Path
+
+# On récupère le dossier où se trouve le script app.py
+BASE_DIR = Path(__file__).resolve().parent
+
+# On construit le chemin vers india.csv (situé dans le même dossier)
+csv_path = BASE_DIR / 'india.csv'
+
+# Lecture du fichier
+df = pd.read_csv(csv_path)
+
+list_of_states = list(df['State'].unique())
+list_of_states.insert(0,'Overall India')
+
+st.sidebar.title('India Ka Data Viz')
+
+selected_state = st.sidebar.selectbox('Select a state',list_of_states)
+primary = st.sidebar.selectbox('Select Primary Parameter',sorted(df.columns[5:]))
+secondary = st.sidebar.selectbox('Select Secondary Parameter',sorted(df.columns[5:]))
+
+plot = st.sidebar.button('Plot Graph')
+
+if plot:
+
+    st.text('Size represent primary parameter')
+    st.text('Color represents secondary parameter')
+    if selected_state == 'Overall India':
+        # plot for india
+        fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=4,size_max=35,
+                                mapbox_style="carto-positron",width=1200,height=700,hover_name='District')
+
+        st.plotly_chart(fig,use_container_width=True)
+    else:
+        # plot for state
+        state_df = df[df['State'] == selected_state]
+
+        fig = px.scatter_mapbox(state_df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=6, size_max=35,
+                                mapbox_style="carto-positron", width=1200, height=700,hover_name='District')
+
+        st.plotly_chart(fig, use_container_width=True)
+
+
+
