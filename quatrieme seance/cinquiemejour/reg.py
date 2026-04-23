e53c16850455de729d143a4a37b1abf2912e4617
3Malaika <babouma.suzanne@institutsaintjean.org>
2026-04-23 14:49:02 +0100

    add cinquiemejour

---
+++ quatrieme seance/cinquiemejour/reg.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle 
+from sklearn.preprocessing import LabelEncoder
+import time 
+
+
+import os
+BASE_DIR = os.path.dirname(__file__)
+with open(os.path.join(BASE_DIR, '../../Second-jour-cour/reg.pkl'), 'rb') as file:
+    model = pickle.load(file)
+
+st.set_page_config(page_title="Predicteur de charges medicales")
+st.title("Prediction de charges medicales")
+st.markdown("Remplis les informations ci dessous pour predire tes charges")
+
+col1,col2 = st.columns(2)
+with col1:
+    age = st.slider('Age',18,100,30)
+with col2:
+    sex = st.selectbox('Sexe',["male","female"])
+
+
+col3,col4 = st.columns(2)
+with col3:
+    bmi = st.number_input("BMI (indice de masse corporelle)",10,50,25)
+with col4:
+    children = st.slider('Nombre d enfants',0,5,1)
+    
+
+col5,col6 = st.columns(2)
+with col5:
+    smoker = st.selectbox('Fumeur',["yes","no"])
+with col6:
+    region = st.selectbox('Region',["southwest","southeast","northwest","northeast"])
+
+sex_encoded = 1 if sex == "male" else 0
+smoker_encoded = 1 if smoker=="yes" else 0
+region_dict = {"southwest": 0.24308153,"southeast": 0.27225131,"northwest":0.24233358,"northeast":0.27225131}
+region_encoded = region_dict[region]
+
+input_data = [[age,sex_encoded,bmi,children,smoker_encoded,region_encoded]]
+
+if st.button('Prediction des charges medicales'):
+    with st.spinner('Calcul  en cours ....'):
+        prediction = model.predict(input_data)[0]
+        time.sleep(1)
+    st.success('Prediction terminée')
+    st.markdown(f"#### Charges medicales Estimées: **${prediction:,.2f}**")
+    st.balloons()
